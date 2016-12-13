f942c381cd1e0d49ce7a9641a4db5093ba3ebdee
Phan Van Duc <Phan Van Duc>
2016-12-13 23:54:52 +0700

    Initial commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+pd.set_option('display.width', 320)
+df = pd.read_csv('E:\\Big Data - Data Mining\\pluralsight_Understand_Machine_Learning\\python-understanding-machine-learning\\4-python-understanding-machine-learning-m4-exercise-files\\data\\pima-data.csv')
+
+df.shape
